29f12f46b339f50822b7fae0d8ded2c9a1ad70ff
Levin Weinstein <weinsteinlevin@gmail.com>
2019-11-20 00:22:47 -0800

    Update Hashtable.java
    Alphabetize methods
    Add javadoc for each method

--- Hashtable.java
+++ Hashtable.java
@@ -37,7 +37,46 @@ public class Hashtable {
             innerArray[i] = new LinkedList<>();
     }
 
+    /**
+     * containsKey: determine whether the Hashtable contains a given key.
+     *
+     * @param key
+     *          The key to check for in the table
+     * @return
+     *          Whether or not the key is in the table
+     */
+    public boolean containsKey(String key){
+        List<HashtableEntry> chain = innerArray[Math.abs(key.hashCode()) % ARRAY_SIZE];
 
+        for (HashtableEntry entry : chain){
+            if (entry.key.equals(key))
+                return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * get: gets the value for a given key from the Hashtable
+     *
+     * @param key
+     *         The key to get the corresponding value of from the table.
+     * @return
+     *         The value corresponding to {key} in the table, or null if key not in table.keys.
+     */
+    public String get(String key){
+        List<HashtableEntry> chain = innerArray[Math.abs(key.hashCode()) % ARRAY_SIZE];
+
+        for (HashtableEntry entry : chain){
+            if (entry.key.equals(key))
+                return entry.val;
+        }
+        return null;
+    }
+
+    /**
+     * put: Puts the value {value} at the key {key}.
+     */
     public void put(String key, String value){
         List<HashtableEntry> chain = innerArray[Math.abs(key.hashCode()) % ARRAY_SIZE];
 
@@ -53,16 +92,13 @@ public class Hashtable {
         chain.add(newEntry);
     }
 
-    public String get(String key){
-        List<HashtableEntry> chain = innerArray[Math.abs(key.hashCode()) % ARRAY_SIZE];
-
-        for (HashtableEntry entry : chain){
-            if (entry.key.equals(key))
-                return entry.val;
-        }
-        return null;
-    }
-
+    /**
+     * Remove: removed a key/value pair from the Hashtable.
+     *
+     * @param key
+     *          the key to remove from the hashtable
+     * @return
+     */
     public String remove(String key){
 
         List<HashtableEntry> chain = innerArray[Math.abs(key.hashCode()) % ARRAY_SIZE];
@@ -77,13 +113,4 @@ public class Hashtable {
         return null;
     }
 
-    public boolean containsKey(String key){
-        List<HashtableEntry> chain = innerArray[Math.abs(key.hashCode()) % ARRAY_SIZE];
-
-        for (HashtableEntry entry : chain){
-            if (entry.key.equals(key))
-                return true;
-        }
-        return false;
-    }
 }
